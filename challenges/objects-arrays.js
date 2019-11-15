68903454ab156a30ade631fd8768d0e7c0035f88
Milo Rastgoo <hello@informagician.com>
2019-11-15 11:50:59 -0500

    universities completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -72,7 +72,10 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates.map(function(cur){
+  return cur.university;
+});
+universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -81,11 +84,15 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(function(cur){
+  return `${cur.first_name} ${cur.email}`;
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter(function(cur){
+  return cur.university.includes('Uni');
+});
 console.log(unisWithUni);
 
 
@@ -112,6 +119,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(item){
+  displayNames.push(`Name:${item.animal_name}, Scientific:${item.scientific_name}.`);
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -120,7 +130,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(cur){
+  return cur.animal_name.toLowerCase();
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
